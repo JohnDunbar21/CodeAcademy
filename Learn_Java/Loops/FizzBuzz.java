6cde7da6df8c36110c3f34df328ea96a391a8e8f
John Dunbar <u08jd21@abdn.ac.uk>
2022-07-30 22:48:44 +0100

    java fizzbuzz fixed

--- Learn_Java/Loops/FizzBuzz.java
+++ Learn_Java/Loops/FizzBuzz.java
@@ -5,11 +5,13 @@ public class FizzBuzz {
     public static void main(String[] args){
         for (int i = 1; i <= 100; i++){
             if (i % 3 == 0){
-                System.out.println("Fizz");
+                if (i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                }
             } else if (i % 5 == 0){
                 System.out.println("Buzz");
-            } else if (i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
             } else {
                 System.out.println(i);
             }
